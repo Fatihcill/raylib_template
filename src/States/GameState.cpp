132d121e1df3189b09ce5d714a67ebec53b7cbcc
Fatihcil16 <fatihcil.2001@gmail.com>
2021-10-24 17:17:00 +0300

    Bug Fixed

--- src/States/GameState.cpp
+++ src/States/GameState.cpp
@@ -5,7 +5,8 @@ void GameState::initVariables()
     this->target = LoadRenderTexture(this->stateData->virtualwindow_width, this->stateData->virtualwindow_height); // 800 600 beginning resolutions of game look Game.cpp for change that
     //setup camera
     cameramanager.setupCamera();
-    virtualratio = GetScreenWidth() / this->stateData->virtualwindow_height; // virtual ratio calc
+    virtualratio = this->stateData->windowSettings.GetResolution().x / this->stateData->virtualwindow_height; // virtual ratio calc
+    cameramanager.UpdateCameraCenter(Vector2{this->stateData->windowSettings.GetResolution().x / 2.f, this->stateData->windowSettings.GetResolution().y / 2.f});
 }
 
 GameState::GameState(StateData *state_data) : State(state_data)
@@ -28,14 +29,17 @@ void GameState::update(const float &dt)
 {
     if (this->paused)
     {
-        virtualratio = GetScreenWidth() / this->stateData->virtualwindow_height;
+        virtualratio = this->stateData->windowSettings.GetResolution().x / this->stateData->virtualwindow_height;
         return; //if the game paused. its return.
     }
     this->updateInput(dt);
     player.move();
 
+    std::cout << "vt : " << virtualratio << std::endl;
+    std::cout << "rayW: " << GetScreenWidth() << std::endl;
+    std::cout << "SettingsW: " << this->stateData->windowSettings.GetResolution().x << std::endl;
     // change update worldcamera center
-    cameramanager.UpdateCameraCenter(player.worldpos);
+    cameramanager.UpdateCameraCenter(player.worldpos); // comment this if you wanna static map
 }
 
 void GameState::draw()
@@ -58,7 +62,8 @@ void GameState::render()
     ClearBackground(BLACK); // outside of world camera will be black 
     BeginMode2D(cameramanager.screenspacecamera);
     DrawTexturePro(target.texture, Rectangle{0.0f, 0.0f, (float)target.texture.width, -(float)target.texture.height},
-                   Rectangle{-virtualratio, -virtualratio, GetScreenWidth() + (virtualratio * 2), GetScreenHeight() + (virtualratio * 2)},
+                   Rectangle{-virtualratio, -virtualratio, this->stateData->windowSettings.GetResolution().x + (virtualratio * 2),
+                             this->stateData->windowSettings.GetResolution().y + (virtualratio * 2)},
                    Vector2{0, 0}, 0.f, WHITE);
     EndMode2D();
 
